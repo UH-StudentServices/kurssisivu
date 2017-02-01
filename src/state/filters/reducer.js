9de2dcc2b38de60de49addd459a3ec066ef922ff
Kalle O Ilves <kalleilv@mba-13.cs.helsinki.fi>
2017-02-01 15:07:13 +0200

    better semester ranges

--- src/state/filters/reducer.js
+++ src/state/filters/reducer.js
@@ -5,15 +5,36 @@ import moment from 'moment';
 
 import { UPDATE_YEAR, UPDATE_SEMESTERS, UPDATE_LANGUAGES } from './actions';
 
-function getSemester(): string {
-    const month = moment().get('months');
+function withinDates(date: Date, dates: Date[]): boolean {
+    const [start, end] = dates;
 
-    if (month >= 0 && month < 7) {
-        return 'spring'
-    } else if (month >= 7 && month < 9) {
-        return 'summer';
+    if(+date >= +start && +date < end) {
+        return true;
     } else {
-        return 'autumn';
+        return false;
+    }
+}
+
+function getSemester(): string {
+    const now = moment().toDate();
+    const year = moment().get('years');
+
+    const autumnRange = [
+        new Date(year, 8, 5),
+        new Date(year, 11, 18),
+    ];
+
+    const springRange = [
+        new Date(year - 1, 11, 18),
+        new Date(year, 4, 7),
+    ];
+
+    if (withinDates(now, autumnRange)) {
+        return 'autumn'
+    } else if (withinDates(now, springRange)) {
+        return 'spring';
+    } else {
+        return 'summer';
     }
 }
 
