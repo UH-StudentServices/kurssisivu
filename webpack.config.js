dffd4fda2315e6b3d90f34022c7fdc89255dc2fb
eero3 <eero.siren@kolumbus.fi>
2018-02-26 09:03:25 +0200

    - Updated webpack to support multiconfig & multiple variants in build

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,56 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 
-const isDevelopment = process.env.NODE_ENV === 'development';
+const createVariants = require('parallel-webpack').createVariants;
+const baseOptions = {
+    isDevelopment: process.env.NODE_ENV === 'development'
+};
 
-module.exports = {
-    watch: isDevelopment,
-    entry: path.join(__dirname, 'src', 'index.jsx'),
-    devtool: isDevelopment ? 'eval-source-map': '',
-    output: {
+const variants = {
+  target: ['commonjs2', 'var', 'umd', 'amd']
+};
+
+function createConfig(options) {
+    return  {
+      watch: options.isDevelopment,
+      entry: path.join(__dirname, 'src', 'index.jsx'),
+      devtool: options.isDevelopment ? 'eval-source-map': '',
+      output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'app.js',
+        filename: 'app.' + options.target + '.js',
         publicPath: 'dist',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
+        libraryTarget: options.target
+      },
+      module: {
         rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: {
-                    loader: 'babel-loader'
-                },
-                exclude: /(node_modules|dist)/,
+          {
+            test: /\.(js|jsx)$/,
+            use: {
+              loader: 'babel-loader'
             },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({ loader: 'css-loader!postcss-loader!sass-loader' })
-            }
+            exclude: /(node_modules|dist)/,
+          },
+          {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({ loader: 'css-loader!postcss-loader!sass-loader' })
+          }
         ],
-    },
-    resolve: {
+      },
+      resolve: {
         extensions: ['.js', '.jsx', '.scss'],
         modules: [path.join(__dirname, 'node_modules'), path.join(__dirname, 'src')],
-    },
-    plugins: [
+      },
+      plugins: [
         new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-                ASSETS_URL: JSON.stringify(isDevelopment ? '/dist' : 'https://www.cs.helsinki.fi/u/mluukkai/cdn'),
-            },
+          'process.env': {
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+            ASSETS_URL: JSON.stringify(options.isDevelopment ? '/dist' : 'https://www.cs.helsinki.fi/u/mluukkai/cdn'),
+          },
         }),
         new ExtractTextPlugin('app.css'),
-        isDevelopment ? null : new webpack.optimize.UglifyJsPlugin({ minimize: true }),
-    ].filter(p => !!p),
-};
+        options.isDevelopment ? null : new webpack.optimize.UglifyJsPlugin({ minimize: true }),
+      ].filter(p => !!p)
+    }
+}
+
+module.exports = createVariants(baseOptions, variants, createConfig)
